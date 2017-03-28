46da995c04983d4aefe5d0a74b0bb480dabbd6c2
Peter Spacek <petersp@stud.ntnu.no>
2017-03-28 11:00:51 +0200

    commit

--- app/src/main/java/peter/ruza/ProfileActivity.java
+++ app/src/main/java/peter/ruza/ProfileActivity.java
@@ -2,8 +2,13 @@
         package peter.ruza;
 
         import android.content.Intent;
+        import android.support.design.widget.NavigationView;
+        import android.support.v4.widget.DrawerLayout;
+        import android.support.v7.app.ActionBar;
+        import android.support.v7.app.ActionBarDrawerToggle;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.view.MenuItem;
         import android.view.View;
         import android.widget.Button;
         import android.widget.TextView;
@@ -20,6 +25,9 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
     //view objects 
     private TextView textViewUserEmail;
     private Button buttonLogout;
+    private DrawerLayout drawerLayout;
+    private TextView logoutMenu;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
 
 
     @Override
@@ -29,10 +37,14 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
         //initializing firebase authentication object
         firebaseAuth = FirebaseAuth.getInstance();
-
+        drawerLayout = (DrawerLayout) findViewById(R.id.profile_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         //if the user is not logged in
         //that means current user will return null
-        if(firebaseAuth.getCurrentUser() == null){
+        if (firebaseAuth.getCurrentUser() == null) {
             //closing this activity
             finish();
             //starting login activity
@@ -45,13 +57,40 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         //initializing views
         textViewUserEmail = (TextView) findViewById(R.id.textViewUserEmail);
         buttonLogout = (Button) findViewById(R.id.buttonLogout);
-
+        logoutMenu = (TextView) findViewById(R.id.menu_logout);
         //displaying logged in user name
-        textViewUserEmail.setText("Welcome "+user.getEmail());
+        textViewUserEmail.setText("Welcome " + user.getEmail());
         //adding listener to button 
         buttonLogout.setOnClickListener(this);
-    }
+        //    logoutMenu.setOnClickListener(this);
+        /*
+        mNavigationView = (NavigationView) findViewById(R.id.nav_view);
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(final MenuItem menuItem) {
 
+                int id = menuItem.getItemId();
+                switch (id) {
+                    case R.id.nav_account:
+                        Toast.makeText(MainActivity.this, "Account Selected", Toast.LENGTH_SHORT).show();
+                        return true;
+
+                    case R.id.nav_settings:
+                        Toast.makeText(MainActivity.this, "Setting Selected", Toast.LENGTH_SHORT).show();
+
+                    default:
+                        return true;
+                }
+
+            }
+            */
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item))
+            return true;
+        return super.onOptionsItemSelected(item);
+    }
     @Override
     public void onClick(View view) {
         //if logout is pressed
@@ -61,6 +100,14 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
             //closing activity
             finish();
             //starting login activity 
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+        if(view == logoutMenu){
+            //logging out the user
+            firebaseAuth.signOut();
+            //closing activity
+            finish();
+            //starting login activity
             startActivity(new Intent(this, LoginActivity.class));
         }
     }
